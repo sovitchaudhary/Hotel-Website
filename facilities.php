e98ac3f67f96130be335bf23491bc17b07679b7a
sovitchaudhary <sovitchaudhary45@gmail.com>
2024-02-28 11:17:13 +0530

    Add new file

---
+++ facilities.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Basera</title>
+
+    <?php include './includes/links.php'; ?>
+
+    <!-- External css link -->
+    <link rel="stylesheet" type="text/css" href="./css/style.css" />
+
+    </style>
+</head>
+
+<body>
+    <?php include './includes/header.php'; ?>
+
+    <div class="container-fluid">
+        <!-- Aside left -->
+        <div class="row mx-lg-3 my-lg-5">
+            <div class="col-lg-3 px-lg-0 px-md-0 bg-dark text-center text-white px-5">
+                aside1
+            </div>
+
+            <!-- Our Facilities -->
+
+            <div class="col-lg-9 px-5">
+                <h2 class="pt-4 mb-4 text-center fw-bold h-font">OUR FACILITIES</h2>
+                <div class="row justify-content-evenly px-lg-0 px-md-0 px-5">
+                    <div class="col-lg-2 col-md-2 text-center bg-white shadow rounded py-4 my-3">
+                        <img src="img/wifi.svg" alt="" width="80px" />
+                        <h5 class="mt-3">WiFi</h5>
+                    </div>
+                    <div class="col-lg-2 col-md-2 text-center bg-white shadow rounded py-4 my-3">
+                        <img src="img/wifi.svg" alt="" width="80px" />
+                        <h5 class="mt-3">WiFi</h5>
+                    </div>
+                    <div class="col-lg-2 col-md-2 text-center bg-white shadow rounded py-4 my-3">
+                        <img src="img/wifi.svg" alt="" width="80px" />
+                        <h5 class="mt-3">WiFi</h5>
+                    </div>
+                    <div class="col-lg-2 col-md-2 text-center bg-white shadow rounded py-4 my-3">
+                        <img src="img/wifi.svg" alt="" width="80px" />
+                        <h5 class="mt-3">WiFi</h5>
+                    </div>
+                    <div class="col-lg-2 col-md-2 text-center bg-white shadow rounded py-4 my-3">
+                        <img src="img/wifi.svg" alt="" width="80px" />
+                        <h5 class="mt-3">WiFi</h5>
+                    </div>
+                    <div class="col-lg-12 text-center mt-5">
+                        <a href="#" class="btn btn-sm btn-outline-dark rounded-0 fw-bold shadow-none">More
+                            Facilities >>></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
